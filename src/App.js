91b9efd9c220edb11f80467aa0ea5ac129bc5654
Crystal <crystalwatkins428@gmail.com>
2020-08-06 00:03:03 -0400

    updated files and initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container-fluid">
+        <Switch>
+          <Route path="/Portfolio" component={Portfolio}></Route>
+          <Route path="/Contact" component={Contact}></Route>
+          <Route path="/About" component={About}></Route>
+          <Route path="/" component={About}></Route>
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
