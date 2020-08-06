ac3b46167ecdf286d6318fafdb1215dc4d877534
Crystal <crystalwatkins428@gmail.com>
2020-08-06 00:06:42 -0400

    Updates

---
+++ precache-manifest.9dac1f6184b2749a8366413de245cb92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffb388f002391230692b4d0e89cc8d8",
+    "url": "/crystal-watkins-portfolio/index.html"
+  },
+  {
+    "revision": "766811546a02cbc42a93",
+    "url": "/crystal-watkins-portfolio/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "766811546a02cbc42a93",
+    "url": "/crystal-watkins-portfolio/static/js/2.15ecebf6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/crystal-watkins-portfolio/static/js/2.15ecebf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fab91d4f8a1056d7e71",
+    "url": "/crystal-watkins-portfolio/static/js/main.98d71f37.chunk.js"
+  },
+  {
+    "revision": "e5f0836553c61354fdf4",
+    "url": "/crystal-watkins-portfolio/static/js/runtime-main.3d92ce78.js"
+  }
+]);
