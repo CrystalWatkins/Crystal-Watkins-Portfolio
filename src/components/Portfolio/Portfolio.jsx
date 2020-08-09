a8cefc691f0ca8077238aa4f7ba8c33a3cddd4ef
Crystal <crystalwatkins428@gmail.com>
2020-08-09 15:16:00 -0400

    updated pictures for portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -20,10 +20,28 @@ const Portfolio = () => {
                 rounded
                 fluid
               />
-              <Fade/>
+              <Fade />
               <figcaption>Done n Dusted</figcaption>
+              <p>
+                Description: Once you log on to our app, you can upload a
+                receipt from any store. You will then receive an itemized list
+                of your purchases that you can edit and take on your next trip
+                to the store!
+              </p>
+              <p>
+                Technology: Veryfi API, dbdiagram.io, Sketch, Materializecss,
+                NPM packages: express, express-handlebars, passport, mysql 2,
+                express-session, bcrypt, @handlebars/allow-prototype-access,
+                Sequelize, MVC, CRUD{" "}
+              </p>
+              <a href="https://fathomless-sands-26873.herokuapp.com/">
+                Deployed Website
+              </a>
+              <br></br>
+              <a href="https://github.com/CrystalWatkins/grocr-receipt-app">
+                Github Repo
+              </a>
             </Figure>
-            <a href="github.com">Done n Dusted</a>
           </div>
           <div className="col-sm-6">
             <Figure>
@@ -31,33 +49,67 @@ const Portfolio = () => {
                 className="portfolio-pic"
                 src="https://github.com/CrystalWatkins/Crystal-Watkins-Portfolio/blob/master/public/Images/EmployeeMngmt.png?raw=true"
                 rounded
-                alt="Employee Management"
+                alt="Employee Summary"
                 fluid
               />
-                          <Fade/>
-              <figcaption>Employee Management</figcaption>
+              <Fade />
+              <figcaption>Object Oriented Program Employee Summary</figcaption>
+              <p>
+                Description: This is a homework requirement for GT Full Stack
+                Flex Program. This node express is designed to create a webpage
+                when you fill out a prompt of questions through the terminal. It
+                will ask you multiple questions that you will answer for who is
+                working in your office and their information. Then it will be
+                created or updated under the Output Folder as team.html where
+                the page is created based on the office input!{" "}
+              </p>
+              <p>Technology: Node express, HTML tests, javascript</p>
+              <a href="https://crystalwatkins.github.io/gt-hw-object-oriented-programming-employee-summary/">
+                Deployed Website
+              </a>
+              <br></br>
+              <a href="https://github.com/CrystalWatkins/gt-hw-object-oriented-programming-employee-summary">
+                Github Repo
+              </a>
             </Figure>
-            <a href="github.com">Employee Management</a>
           </div>
         </div>
 
         <div className="row">
-        <div className="col-sm-6">
+          <div className="col-sm-6">
             <Figure>
               <Figure.Image
                 className="portfolio-pic"
                 src="https://github.com/CrystalWatkins/Crystal-Watkins-Portfolio/blob/master/public/Images/Project1.png?raw=true"
                 rounded
-                alt="Project One"
+                alt="Remote island"
                 fluid
               />
-                          <Fade/>
-              <figcaption>Project One- Destination Staycation</figcaption>
-              
+              <Fade />
+              <figcaption>Remote Island</figcaption>
+              <p>
+                Description: Remote Island allows a user to choose a travel
+                destination from a list of options.  Once a location type is
+                selected, the user will be presented with their travel
+                amenities.  Through the use of API’s, the user will view a
+                livestream video, pulled from a location similar to
+                location-type they selected.  The user will also be presented
+                with  a random drink, and optional recipe.  Lastly, the user
+                will be presented with current weather information in relation
+                to location selected.{" "}
+              </p>
+              <p>
+                Technology: OpenWeather API and CocktailDB API, HTML, CSS,
+                Bootstrap for customized styling, and Javascript/jQuery to
+                create dynamic web page. Images from Freepik. Google fonts.
+              </p>
+              <a href="https://crystalwatkins.github.io/gt-project-1/">
+                Deployed Website
+              </a>
+              <a href="https://github.com/CrystalWatkins/gt-project-1">
+                Github Repo
+              </a>
             </Figure>
-            <a href="https://crystalwatkins.github.io/gt-project-1/.">
-              Project One
-            </a>
           </div>
           <div className="col-sm-6">
             <Figure>
@@ -68,16 +120,28 @@ const Portfolio = () => {
                 alt="Employee Tracker"
                 fluid
               />
-                          <Fade/>
-              <figcaption>Employee Tracker</figcaption>
+              <Fade />
+              <figcaption> My SQL Employee Tracker</figcaption>
+              <p>
+                Description: This mySQL webpage is designed to create an
+                employee database that combines three different tables to be
+                manipulated and changed by the user for the specific needs of a
+                company. You can assign an employee to a different department,
+                get information about specific data within your company, add
+                different aspects to the company and delete employee
+                information.
+              </p>
+              <p>Technology: mySQL and Javascript</p>
+              <a href="https://drive.google.com/file/d/1Xy5LKlEEk7d3nxKSND_KHRTo8ax-7uxZ/view">
+                Video Demonstration
+              </a>
+              <br></br>
+              <a href="https://github.com/CrystalWatkins/gt-hw-my-sql-employee-tracker">
+                Github Repo
+              </a>
             </Figure>
-            <a href="https://github.com/CrystalWatkins/gt-hw-my-sql-employee-tracker">
-              Employee Tracker
-            </a>
           </div>
         </div>
-
-
         <div className="row">
           <div className="col-sm-6">
             <Figure>
@@ -88,12 +152,33 @@ const Portfolio = () => {
                 alt="Burger App"
               />
               <figcaption>Burger App</figcaption>
-              <Fade/>
+              <Fade />
+              <p>
+                Description: This is a homework requirement for GT Full Stack
+                Flex Program. This node express burger app and webpage is
+                designed to utilize server side rendering using MVC. By using
+                handlebars we were asked to create a directory structure for
+                this homework which was followed exactly as asked. When you open
+                the website, you are able to see burgers pop up. You can then
+                add a burger that you want to eat. It will then put it in the
+                "Burgers To Be Eaten" column, where you can choose to "eat" the
+                burger with the touch of a button and then it will move to the
+                "devoured" column. From there, you can say you are still hungry
+                and it will move the burger back to the "Burgers To Be Eaten"
+                column!{" "}
+              </p>
+              <p>
+                Technology: MVC paradigm, javascript, html, heroku, controllers,
+                config, css
+              </p>
+              <a href="https://evening-ravine-28370.herokuapp.com/">
+                Deployed Website
+              </a>
+              <br></br>
+              <a href="https://github.com/CrystalWatkins/gt-hw-mvc-burger-app">
+                Github Repo
+              </a>
             </Figure>
-            <a href="https://evening-ravine-28370.herokuapp.com/">
-              Heroku
-            </a>
-            <a href= "https://github.com/CrystalWatkins/gt-hw-mvc-burger-app"> Github</a>
           </div>
           <div className="col-sm-6">
             <Figure>
@@ -103,15 +188,27 @@ const Portfolio = () => {
                 rounded
                 alt="React User Directory"
               />
-                          <Fade/>
+              <Fade />
               <figcaption>React user directory</figcaption>
+              <p>
+                Description:This is a homework requirement for GT Full Stack
+                Flex Program. We are using react to create an employee user
+                directory. When you click on the name, the list sorts
+                alphabetically by the first name. When you search, you receive
+                the search for the employee you searched. With more, the app
+                will be destructured to be more re-usable. This is calling on
+                the API random user.
+              </p>
+              <p>Technology: React</p>
+              <a href="https://crystalwatkins.github.io/gt-hw-react-user-directory-app/">
+                Deployed Website
+              </a>
+              <br></br>
+              <a href="https://github.com/CrystalWatkins/gt-hw-react-user-directory-app">
+                Github Repo
+              </a>
             </Figure>
-            <a href="https://crystalwatkins.github.io/gt-hw-react-user-directory-app/">
-             Deployed Site
-            </a>
-            <a href= "https://github.com/CrystalWatkins/gt-hw-react-user-directory-app"> Github</a>
           </div>
-
         </div>
       </div>
     </div>
