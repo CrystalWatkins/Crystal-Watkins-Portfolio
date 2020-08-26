da5939660510842cca4214ef89a05fe31d3e678e
Crystal <crystalwatkins428@gmail.com>
2020-08-26 10:41:52 -0400

    updated contact page to just form- and restyled for cleaner look

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,38 +10,7 @@ render() {
         <div className=" col-sm-12 jumbotron">
           <h1 className="contact">Contact</h1>
           <hr className="my-4" />
-
-          
-
         <div className="row">
-        <div className="col-sm-6">
-        <label className="label is-large ml-4">
-              <button className="fas fa-book"></button> Email
-            </label>
-      <br />
-
-        <button type="button" className="btn btn-default btn-lg">
-        <span className="glyphicon glyphicon-earphone" aria-hidden="true"></span> Phone
-        </button>
-
-
-        <br />
-
-        <button type="button" className="btn btn-default btn-lg">
-        <span className="glyphicon glyphicon-#0077b5" aria-hidden="true"></span> LinkedIn
-        </button>
-
-        <br />
-
-        <button type="button" className="btn btn-default btn-lg">
-        <span className="glyphicon glyphicon-#24292e" aria-hidden="true"></span> Github
-        </button>
-
-
-
-
-        </div>
-
           <div className="col-sm-6">
             <form>
             <div className="form-group">
@@ -54,6 +23,7 @@ render() {
             id="exampleFormControlInput1"
             placeholder="John Smith"
           ></input>
+          <br />
           <div className="form-group">
             <label htmlFor="exampleFormControlInput1">Email</label>
             <input
@@ -63,6 +33,7 @@ render() {
               placeholder="example@gmail.com"
             />
           </div>
+          <br />
           <div className="form-group">
             <label htmlFor="exampleFormControlTextarea1">Message</label>
             <textarea
@@ -72,6 +43,7 @@ render() {
               placeholder="Message"
             ></textarea>
           </div>
+          <br />
           <button type="submit" className="btn btn-secondary">
             Submit
           </button>
